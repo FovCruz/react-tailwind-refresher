75cdfd179e33d1bd8afa2c6de36af651c31f2a6a
Fabián Cruz <fov.cruz@gmail.com>
2025-11-22 15:55:26 +0000

    First commit del proyecto completo (frontend, backend, nginx)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1 className="text-3xl text-red-500">Home</h1>} />
+      <Route path="/about" element={<h1>About</h1>} />
+      <Route path="/contact" element={<h1>Contact</h1>} />
+    </Routes>
+  );
+}
+
+export default App;
